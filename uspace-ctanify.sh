4e317ed3d666c1f2dff312282228809a89b5ffc3
Vaclav Haisman <vhaisman@gmail.com>
2016-10-05 14:58:57 +0200

    Documnatation and CTAN packaging script.

---
+++ uspace-ctanify.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -x
+set -e
+
+latexmk -gg -xelatex -interaction=nonstopmode uspace-test.tex >uspace-test.tex.output 2>&1 </dev/null &
+
+latexmk -gg -lualatex -interaction=nonstopmode uspace.tex >uspace.tex.output 2>&1 </dev/null &
+
+echo waiting for jobs to finish
+wait
+
+cat uspace-test.tex.output
+cat uspace.tex.output
+
+DOCDIR=doc/latex/uspace
+LATEXDIR=tex/latex/uspace
+
+TEMP_DIR=`mktemp -d -p "$PWD"`
+read -d '' CTANIFY_MAP <<EOF || true
+           uspace.sty=$LATEXDIR 
+           README.md=$DOCDIR 
+           LICENSE=$DOCDIR 
+           uspace-test.tex=$DOCDIR 
+           uspace-test.pdf=$DOCDIR 
+           uspace.tex=$DOCDIR 
+           uspace.pdf=$DOCDIR
+EOF
+
+echo "map:" $CTANIFY_MAP
+
+for entry in $CTANIFY_MAP ; do
+    file=${entry%=*}
+    #target_dir=${entry#*=}
+    cp -v "$file" "$TEMP_DIR"
+done
+
+ROOT_DIR="$PWD"
+(
+    cd "$TEMP_DIR"
+    setfacl -b *
+    chmod +rw-x *
+    ctanify --pkgname=uspace $CTANIFY_MAP
+    mv -vf uspace.tar.gz "$ROOT_DIR"
+)
+        
+tar tvvzf uspace.tar.gz
+
+rm -rf "$TEMP_DIR"
